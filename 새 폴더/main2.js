d6218b3e3cfdef1fbf44b30b0aab38a3bca42b2a
junbeom07 <133008219+junbeom07@users.noreply.github.com>
2023-10-13 16:32:30 +0900

    자바 스크립트 추가

---
+++ 새 폴더/main2.js
@@ -0,0 +1,10 @@
+const comPic =document.querySelector(".comPic");
+const myPicR =document.querySelector(".myPicR");
+const myPicS =document.querySelector(".myPicS");
+const myPicP =document.querySelector(".myPicP");
+const forMain2 = document.querySelector(".forMain2");
+
+const RSP = ["묵", "찌", "빠"];
+const caseR = { 묵: "비겼다", 찌:"이겼다", 빠:"졌다"};
+const cases = { 묵: "졌다", 찌:"비겼다", 빠:"이겼다"};
+const caseP = { 묵: "이겼다", 찌:"졌다", 빠:"비겼다"};
